e202b9e00c6489929092c88a9339cfe1e5185e81
Mike Sue-Ping <msueping@lwolf.com>
2018-02-05 15:52:51 -0500

    Added vertical scrollbar on bootstrap table of stories

--- src/Components/StoryList.js
+++ src/Components/StoryList.js
@@ -15,7 +15,8 @@ export default class StoryList extends React.Component {
             <BootstrapTable data={ this.props.stories }
                             headerStyle={ { border: 'red 0px solid' } }
                             bodyStyle={ { border: 'green 0px solid' } }
-                            selectRow={selectRow}>
+                            selectRow={selectRow}
+                            maxHeight='300px'>
                 <TableHeaderColumn dataField='id' isKey={ true } hidden={true} dataAlign='center'>Story</TableHeaderColumn>
                 <TableHeaderColumn width='80%' dataAlign='left' dataField='story'>Story</TableHeaderColumn>
                 <TableHeaderColumn width='20%' dataAlign='center' dataField='avg'>Avg.</TableHeaderColumn>
